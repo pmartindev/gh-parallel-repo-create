373cfc3444e49a764a3f6f8e1c5544816df00ff8
Preston Martin <pmartindev@github.com>
2023-03-29 17:46:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# gh-parallel-ts-template
+A template for a Typescript app that will execute parallel github api calls. It can be ran as either a Docker container, or as a Node app.
+
+Features of this type of repository include:
+1. Devcontainer for running & testing a TS app
+2. Ability to be ran via docker or node
+3. Ability to be ran with command line arguments, environment variables, or a .env file
+
+To get started:
+1. Start the codespace
+2. Populate the .env file with the correct values
+3. `npm start`
---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import { Octokit } from "@octokit/rest";
+import dotenv from "dotenv";
+const yargs = require('yargs')
+
+const collectResults = async() => {
+    dotenv.config();
+    const { endpoint, token } = acceptCommandLineArgs();
+    const octokit: Octokit = new Octokit({
+        auth: token,
+        baseUrl: endpoint
+    });
+    const promises: Promise<string>[] = Array.from({ length: 100 }, () => runApiRequest(octokit));
+    return Promise.all(promises);
+};
+
+const run = async () => {
+    console.log('Collecting results...');
+    const results: string[] = await collectResults();
+    console.log(results);
+    console.log('Done');
+  };
+
+async function runApiRequest(octokit: Octokit) {
+    const result: string = (await octokit.users.getAuthenticated()).data.login;
+    return result;
+}
+
+export function acceptCommandLineArgs(): { endpoint: string, token: string } {
+    const argv = yargs.default(process.argv.slice(2))
+        .env('GITHUB')
+        .option('endpoint', {
+            alias: 'e',
+            env: 'GITHUB_ENDPOINT',
+            description: 'The api endpoint of the github instance (ex. api.github.com)',
+            type: 'string',
+            demandOption: true,
+        })
+        .option('token', {
+            alias: 't',
+            env: 'GITHUB_TOKEN',
+            description: 'The personal access token for the GHES Migration API.',
+            type: 'string',
+            demandOption: true,
+        }).argv;
+    return { endpoint: argv.endpoint, token: argv.token };
+}
+
+// The main entrypoint for the application
+run();
