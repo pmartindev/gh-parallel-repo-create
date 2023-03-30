23c6ee48d0da12267f75c343eacdbb874d96bf60
Preston Martin <pmartindev@github.com>
2023-03-30 03:32:23 +0000

    Added parallel push

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,128 @@
 import { Octokit } from "@octokit/rest";
 import dotenv from "dotenv";
 import randomwords from "random-words";
-import simpleGit, { SimpleGit } from 'simple-git';
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import fs from 'fs';
+import path from "path";
 const yargs = require('yargs')
 
-const collectResults = async () => {
-    dotenv.config();
-    const { 
-        endpoint, 
-        authToken, 
-        org,
-        repoUrls
-     } = acceptCommandLineArgs();
-    const octokit: Octokit = new Octokit({
-        auth: authToken,
-        baseUrl: endpoint
-    });
-    const clonePromises: Promise<string>[] = repoUrls.map((repoUrl: string) => cloneRepo(octokit, repoUrl));
-    const apiPromises: Promise<string>[] = Array.from({ length: 5 }, () => runApiRequest(octokit, org));
-    return Promise.all([...apiPromises, ...clonePromises]);
+const cloneAndCreateRepos = async (gitReposDir: string, octokit: Octokit, args: {
+    apiEndpoint: string;
+    endpoint: string;
+    authToken: string;
+    org: string;
+    repoUrls: string[];
+}) => {
+    await createOrDeleteDir(gitReposDir);
+    const { apiEndpoint, endpoint, authToken, org, repoUrls } = args;
+    const clonePromises: Promise<string>[] = repoUrls.map((repoUrl: string) => cloneRepo(repoUrl, gitReposDir));
+    const apiPromises: Promise<string>[] = Array.from({ length: 5 }, () => createOrgRepo(octokit, org));
+    // gather all promise results and filter out empty strings
+    const allPromiseResults = (await Promise.all([...apiPromises, ...clonePromises])).filter((result: string) => result !== "");
+    return allPromiseResults
 };
 
+const pushClonedRepos = async (gitReposDir: string, octokit: Octokit, createdRepos: string[], args: {
+    apiEndpoint: string;
+    endpoint: string;
+    authToken: string;
+    org: string;
+    repoUrls: string[];
+}) => {
+    const { apiEndpoint, endpoint, authToken, org, repoUrls } = args;
+    // read all of the clone dirs in the repos dir
+    const clonedRepos = fs.readdirSync(gitReposDir, { withFileTypes: true }).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);;
+
+    const repos = createdRepos.map((repo, index) => {
+        const clonedRepo = clonedRepos[index % clonedRepos.length];
+        return { createdRepo: repo, clonedRepo };
+      });
+
+    const pushPromises: Promise<string>[] = repos.map(({ createdRepo, clonedRepo }) => pushRepoToRemote(clonedRepo, createdRepo, endpoint, authToken, org, gitReposDir));
+    const pushPromiseResults = await Promise.all(pushPromises);
+    await createOrDeleteDir(gitReposDir);
+    return pushPromiseResults;
+};
+
+async function pushRepoToRemote(clonedRepo: string, createdRepo: string, endpoint: string, authToken: string, org: string, gitReposDir: string) {
+    const git: SimpleGit = simpleGit(`${gitReposDir}/${clonedRepo}`);
+    const defaultBranch = await git.branchLocal();
+    console.log(`Pushing ${clonedRepo} to https://${endpoint}/${org}/${createdRepo}..`);
+    await git.push(`https://ghe-admin:${authToken}@${endpoint}/${org}/${createdRepo}.git`, `${defaultBranch.current}`);
+    console.log(`Pushed ${clonedRepo} to https://${endpoint}/${org}/${createdRepo}`);
+    return `${org}/${createdRepo}`;
+}
 
 const run = async () => {
+    const gitReposDir: string = path.join(__dirname, "repos");
+    dotenv.config();
+    const { apiEndpoint, authToken, org, repoUrls } = acceptCommandLineArgs();
+    const octokit: Octokit = new Octokit({
+        auth: authToken,
+        baseUrl: apiEndpoint
+    });
     console.log('Collecting results...');
-    const results: string[] = await collectResults();
-    console.log(results);
-    console.log('Done');
+    const createdRepos: string[] = await cloneAndCreateRepos(gitReposDir, octokit, acceptCommandLineArgs());
+    const pushedRepos: string[] = await pushClonedRepos(gitReposDir, octokit, createdRepos, acceptCommandLineArgs());
+    await console.log(pushedRepos);
+    await console.log('Done');
 };
 
-async function runApiRequest(octokit: Octokit, org: string) {
+async function createOrgRepo(octokit: Octokit, org: string) {
     const name = randomwords({ exactly: 2, join: '-' });
     const result: string = (await octokit.repos.createInOrg({
         name,
         org,
         description: 'This is a migration test repo.',
     })).data.name;
+    console.log(`Created repo ${result}`);
     return result;
 }
 
-async function cloneRepo(octokit: Octokit, repoUrl: string) {
+async function cloneRepo(repoUrl: string, gitReposDir: string) {
     const git: SimpleGit = simpleGit();
-    console.log(`Cloning ${repoUrl}...`)
-    const result: string = await git.clone(repoUrl);
-    console.log(`Cloned ${repoUrl}`)
-    return result
+    const repoName: string = repoUrl.split('/').pop() as string;
+    console.log(`Cloning ${repoUrl}...`);
+    await git.clone(repoUrl, `${gitReposDir}/${repoName}`);
+    console.log(`Cloned ${repoUrl} to ${gitReposDir}/${repoName}`);
+    return "";
 }
 
-export function acceptCommandLineArgs(): { 
-    endpoint: string, 
-    authToken: string, 
+const createOrDeleteDir = async (dirPath: string): Promise<void> => {
+    try {
+        if (!fs.existsSync(dirPath)) {
+            await fs.promises.mkdir(dirPath);
+            console.log(`Directory ${dirPath} created!`);
+        } else {
+            console.log(`Directory ${dirPath} already exists, deleting...`);
+            await fs.promises.rm(dirPath, { recursive: true });
+        }
+    } catch (err) {
+        console.error(`Error in createOrDeleteDir: ${err}`);
+        throw err;
+    }
+};
+
+export function acceptCommandLineArgs(): {
+    apiEndpoint: string,
+    endpoint: string,
+    authToken: string,
     org: string,
-    repoUrls: string[] } {
+    repoUrls: string[]
+} {
     const argv = yargs.default(process.argv.slice(2))
         .env('GITHUB')
+        .option('apiEndpoint', {
+            alias: 'a',
+            env: 'GITHUB_API_ENDPOINT',
+            description: 'The api endpoint of the github instance (ex. api.github.com)',
+            type: 'string',
+            demandOption: true,
+        })
         .option('endpoint', {
             alias: 'e',
             env: 'GITHUB_ENDPOINT',
-            description: 'The api endpoint of the github instance (ex. api.github.com)',
+            description: 'The endpoint of the github instance (ex. github.com). NOTE: This is not the api endpoint.',
             type: 'string',
             demandOption: true,
         })
@@ -80,15 +145,10 @@ export function acceptCommandLineArgs(): {
             type: 'string',
             demandOption: true,
         })
-        .option('repoUrls', {
-            alias: 'r',
-            env: 'GITHUB_REPO_URLS',
-            description: 'A comma separated list of repo urls to clone. (ex. https://github.com/torvalds/linux,https://github.com/microsoft/vscode)',
-            type: 'string',
-            demandOption: true,
-        }).argv;
-    return { 
-        endpoint: argv.endpoint, 
+        .argv;
+    return {
+        apiEndpoint: argv.apiEndpoint,
+        endpoint: argv.endpoint,
         authToken: argv.authToken,
         org: argv.org,
         repoUrls: argv.repoUrls.split(',').map((url: string) => url.trim())
